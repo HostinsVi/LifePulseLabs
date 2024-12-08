ed0363c9650f81c7600c5fc60b88ea5b3917d8a6
HostinsVi <vitor.mello@fatec.sp.gov.br>
2024-12-08 19:35:23 -0300

    Dia 4 - SubPaginas, erros, falhas, wasted day

--- public/app.js
+++ public/app.js
@@ -30,3 +30,89 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+
+class SpecialHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <header>
+      <div class="header-left">
+        <div class="header-left-top">
+          <img src="assets/logo.png" class="lpl-logo" alt="LifePulse Labs logo">
+        </div>
+        <div class="header-left-bot">
+          <p>Your Life, Our Pulse</p>
+        </div>
+      </div>
+      <div class="header-right">
+        <div class="header-right-top">
+          <img src="assets/cart.png" class="header-cart" alt="shopping cart">
+        </div>
+        <div class="header-right-bottom">
+          <div class="dropdown" data-dropdown>
+            <button class="link" data-dropdown-button>Chrome</button>
+            <div class="dropdown-menu information-grid">
+              <nav class="dropdown-heading">
+                <nav class="dropdown-links">Operating Systems</nav>
+                <a target="_blank" href="test.html" class="droplink">Biotech</a>
+                <a href="#" class="droplink">Rippler</a>
+                <a href="#" class="droplink">Netdriver</a>      
+              </nav>
+              <nav class="dropdown-heading">
+                <nav class="dropdown-links">Optics</nav>
+                <a href="#" class="droplink">Oracle</a>
+                <a href="#" class="droplink">Sentry</a>
+                <a href="#" class="droplink">Doomsayer</a>
+              </nav>
+              <nav class="dropdown-heading">
+                <nav class="dropdown-links">Arms</nav>
+                <a href="#" class="droplink">MaxTac</a>
+                <a href="#" class="droplink">Monowire</a>
+                <a href="#" class="droplink">Gorilla</a>
+              </nav>
+              <nav class="dropdown-heading">
+                <nav class="dropdown-links">Legs</nav>
+                <a href="#" class="droplink">Lynx</a>
+                <a href="#" class="droplink">Cyber</a>
+                <a href="#" class="droplink">Bionic</a>
+              </nav>
+              <nav class="dropdown-heading">
+                <nav class="dropdown-links">Bio Systems</nav>
+                <a href="#" class="droplink">BioMonitor</a>
+                <a href="#" class="droplink">Nano-Plating</a>
+                <a href="#" class="droplink">Adreno-Trigger</a>
+              </nav>
+            </div>
+            <a href="#" class="link">Dictionary</a>
+            <div class="dropdown second-dropdown" data-dropdown>
+              <button class="link" data-dropdown-button>Trace Ripper</button>
+              <div class="dropdown-menu">
+                <form class="trace-form">
+                  <label for="email">DataPacket(email)</label>
+                  <input type="email" name="email" id="email">
+                  <label for="password">Ripperdoc(any name)</label>
+                  <input type="password" name="password" id="password">
+                  <button type="submit">Ping Doc</button>
+                </form>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    `;
+  }
+}
+
+class SpecialFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <footer>  
+    </footer>
+    `;
+  }
+}
+
+customElements.define('special-header', SpecialHeader)
+customElements.define('special-footer', SpecialFooter)
+
